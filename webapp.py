a3bdc978de0ad5d34a5fe473b2e494925f9c990f
Stephanie Stroud <stephanie.marie.stroud@gmail.com>
2013-10-14 21:32:45 -0700

    One of eight items complete.

--- webapp.py
+++ webapp.py
@@ -7,24 +7,38 @@ app = Flask(__name__)
 def get_github():
     return render_template("get_github.html")
 
+
 @app.route("/student")
 def get_student():
     hackbright_app.connect_to_db()
-    student_github = request.args.get("github")
-    row = hackbright_app.get_student_by_github(student_github)
-    html = render_template("student_info.html", first_name=row[0],
-                                                last_name=row[1],
-                                                github=row[2])
+    github_name = request.args.get("github")
 
+    student = hackbright_app.get_student_by_github(github_name)
+    grades = hackbright_app.show_all_grades(github_name)
+
+    html = render_template("student_info.html",
+        first_name=student[0],
+        last_name=student[1],
+        github=student[2],
+        grades=grades,
+    )
     return html
 
 @app.route("/grade")
 def get_grades():
+    raise TypeError
     hackbright_app.connect_to_db()
-    student_github = request.args.get("github")
-    rows = hackbright_app.show_all_grades(student_github)
-    html = render_template("student_info.html", student_github=student_github,
-                                                grades=rows)
+    github = request.args.get("github")
+
+    student = hackbright_app.get_student_by_github(github)
+    projects = hackbright_app.show_all_grades(github)
+
+    html = render_template("student_info.html",
+        first_name=student[0],
+        last_name=student[1],
+        github=student[2],
+        projects=projects,
+    )
     return html
 
 @app.route("/show_grades") #This one doesn't work yet
